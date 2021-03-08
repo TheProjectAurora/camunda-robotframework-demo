19fb2c0d20a642d067a4ff7c6057a3661132396c
Sakari Hoisko <sakari.hoisko@northcode.fi>
2021-03-08 11:57:15 +0200

    Added camunda-robotframework-demo_credential volume

---
+++ infra/credential-mount-creator/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while [ /bin/true ]
+do
+    rm -rf $2
+    cp -rfv $1/* $2/.
+    content1=$(ls -1 $1)
+    content2=$(ls -1 $1)
+    if [ "$content1" == "$content2" ]; then
+        break;
+    fi
+done
